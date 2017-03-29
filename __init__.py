6ef769c49ba08ee61334fdec4cbedfdda81d96fc
enziop <enzioprobst@gmail.com>
2017-03-29 21:36:43 +0200

    first commit

---
+++ __init__.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+
+'''
+    Copyright (C) 2017  Enzio Probst
+  
+    Created by Enzio Probst
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+bl_info = {
+    "name": "Mixamo Converter",
+    "author": "Enzio Probst",
+    "version": (1, 0, 0),
+    "blender": (2, 7, 8),
+    "location": "3D View > Tool Shelve > Mixamo Tab",
+    "description": ("Script to bake Root motion for Mixamo Animations"),
+    "warning": "",  # used for warning icon and text in addons panel
+    "wiki_url": "",
+    "tracker_url": "" ,
+    "category": "Animation"
+}
+
+import bpy
+from . import mixamoconv
+
+class MixamoconvPanel(bpy.types.Panel):
+    """Creates a Tab in the Toolshelve in 3D_View"""
+    bl_label = "Mixamo Rootbaker"
+    bl_idname = "ANIMATION_PT_mixamo"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'
+    bl_category = "Mixamo"
+
+    def draw(self, context):
+        layout = self.layout
+
+        scene = bpy.context.scene
+        
+        ''' Annoying warning Box
+        #box with general info
+        infobox = layout.box()
+        infobox.label(icon = 'ERROR', text = "Attention:")
+        row = infobox.row()
+        row.label("Batch Convert will delete everything from your current scene")
+        row = infobox.row()
+        row.label("Only use in empty or startup scene")
+        '''
+        
+        box = layout.box()
+        # Options for how to do the conversion
+        row = box.row()
+        row.prop(scene.mixamo, "use_vertical")
+        row.prop(scene.mixamo, "on_ground")
+        # Button for conversion of single Selected rig
+        row = box.row()
+        row.scale_y = 2.0
+        row.operator("mixamo.convertsingle")
+        
+        
+        box = layout.box()
+        # input and output paths for batch conversion
+        box.label(text="Batch")
+        row = box.row()
+        row.prop(scene.mixamo, "inpath")
+        row = box.row()
+        row.prop(scene.mixamo, "outpath")
+        row = box.row()
+        box.prop(scene.mixamo, "force_overwrite")
+        
+        # button to start batch conversion
+        row = box.row()
+        row.scale_y = 2.0
+        row.operator("mixamo.convertbatch")
+        status_row = box.row()
+
+class MixamoPropertyGroup(bpy.types.PropertyGroup):
+    '''Property container for options and paths of mixamo Converter'''
+    use_vertical = bpy.props.BoolProperty(
+                    name="Use Vertical",
+                    description="If enabled, vertical motion is transfered to RootBone",
+                    default=True)
+    on_ground = bpy.props.BoolProperty(
+                    name="On Ground",
+                    description="If enabled, root bone is on ground and only moves up at jumps",
+                    default=True)
+                    
+    force_overwrite = bpy.props.BoolProperty(
+                    name="Force Overwrite",
+                    description="If enabled, overwrites files if output path is the same as input",
+                    default=False)
+    
+    inpath = bpy.props.StringProperty(
+                    name="Input Path",
+                    description="Path to mixamorigs",
+                    maxlen = 256,
+                    default = "",
+                    subtype='FILE_PATH')
+    outpath = bpy.props.StringProperty(
+                    name="Output Path",
+                    description="Where Processed rigs should be saved to",
+                    maxlen = 256,
+                    default = "",
+                    subtype='FILE_PATH')
+
+class OBJECT_OT_ConvertSingle(bpy.types.Operator):
+    '''Button/Operator for converting single Rig'''
+    bl_idname = "mixamo.convertsingle"
+    bl_label = "Convert Single"
+    description = "Bakes rootmotion for a single, already imported rig."
+    
+    def execute(self, context):
+        if bpy.context.object == None:
+            self.report({'ERROR_INVALID_INPUT'}, "Error: no object selected.")
+            return{'CANCELLED'}
+        if bpy.context.object.type != 'ARMATURE':
+            self.report({'ERROR_INVALID_INPUT'}, "Error: %s is not an Armature." % bpy.context.object.name)
+            return{'CANCELLED'}
+        if bpy.context.object.data.bones[0].name != 'mixamorig:Hips':
+            self.report({'ERROR_INVALID_INPUT'}, "Selected object %s is not a Mixamo rig, or at least naming does not match!" % bpy.context.object.name)
+            return{'CANCELLED'}
+        self.report({'INFO'}, "Rig Converted")
+        mixamoconv.HipToRoot(armature = bpy.context.object, use_z = context.scene.mixamo.use_vertical, on_ground = context.scene.mixamo.on_ground)
+        return{'FINISHED'}
+
+class OBJECT_OT_ConvertBatch(bpy.types.Operator):
+    '''Button/Operator for starting batch conversion'''
+    bl_idname = "mixamo.convertbatch"
+    bl_label = "Batch Convert"
+    description = "Converts all mixamorigs from the [Input Path] and exports them to the [Ouput Path]"
+    
+    def execute(self, context):
+        inpath = bpy.context.scene.mixamo.inpath
+        outpath = bpy.context.scene.mixamo.outpath
+        if inpath == '':
+            self.report({'ERROR_INVALID_INPUT'}, "Error: no Input Path set.")
+            return{'CANCELLED'}
+        if outpath == '':
+            self.report({'ERROR_INVALID_INPUT'}, "Error: no Output Path set.")
+            return{'CANCELLED'}
+        if (inpath == outpath) and not bpy.context.scene.mixamo.force_overwrite:
+            self.report({'ERROR_INVALID_INPUT'}, "Input and Output path are the same, source files would be overwritten.")
+            return{'CANCELLED'}
+        if (inpath == outpath) & bpy.context.scene.mixamo.force_overwrite:
+            self.report({'WARNING'}, "Input and Output path are the same, source files will be overwritten.")
+        numfiles = mixamoconv.BatchHipToRoot(bpy.path.abspath(inpath), bpy.path.abspath(outpath), use_z = context.scene.mixamo.use_vertical, on_ground = context.scene.mixamo.on_ground)
+        self.report({'INFO'}, "%d files converted" % numfiles)
+        return{'FINISHED'}
+
+def register():
+    bpy.utils.register_class(MixamoPropertyGroup)
+    bpy.types.Scene.mixamo = bpy.props.PointerProperty(type=MixamoPropertyGroup)
+    bpy.utils.register_class(OBJECT_OT_ConvertSingle)
+    bpy.utils.register_class(OBJECT_OT_ConvertBatch)
+    bpy.utils.register_class(MixamoconvPanel)
+
+def unregister():
+    bpy.utils.unregister_class(MixamoPropertyGroup)
+    bpy.utils.unregister_class(OBJECT_OT_ConvertSingle)
+    bpy.utils.register_class(OBJECT_OT_ConvertBatch)
+    bpy.utils.unregister_class(MixamoconvPanel)
+
+if __name__ == "__main__":
+    register()
+    
+    
---
+++ mixamoconv.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+
+'''
+    Copyright (C) 2017  Enzio Probst
+  
+    Created by Enzio Probst
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import bpy
+import os
+
+'''
+function to bake hipmotion to RootMotion in MixamoRigs
+'''
+def HipToRoot(armature, use_z = True, on_ground = True):
+
+    root = armature
+    root.name = "root"
+    framerange = root.animation_data.action.frame_range
+
+    hips = root.pose.bones["mixamorig:Hips"]
+    z_offset = hips.bone.head.y * root.scale.y
+
+    #Create helper to bake the root motion
+    bpy.ops.object.empty_add(type='PLAIN_AXES', radius=1, view_align=False, location=(0, 0, 0), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
+    rootBaker = bpy.context.object
+    rootBaker.name = "rootBaker"
+
+    if use_z:
+        print("using z")
+        bpy.ops.object.constraint_add(type='COPY_LOCATION')
+        bpy.context.object.constraints["Copy Location"].name = "Copy Z_Loc"
+        bpy.context.object.constraints["Copy Z_Loc"].target = root
+        bpy.context.object.constraints["Copy Z_Loc"].subtarget = "mixamorig:Hips"
+        bpy.context.object.constraints["Copy Z_Loc"].use_x = False
+        bpy.context.object.constraints["Copy Z_Loc"].use_y = False
+        bpy.context.object.constraints["Copy Z_Loc"].use_z = True
+        bpy.context.object.constraints["Copy Z_Loc"].use_offset = True
+        if on_ground:
+            print("using on ground")
+            rootBaker.location[2] = -z_offset
+            bpy.ops.object.constraint_add(type='LIMIT_LOCATION')
+            bpy.context.object.constraints["Limit Location"].use_min_z = True        
+
+    bpy.ops.object.constraint_add(type='COPY_LOCATION')
+    bpy.context.object.constraints["Copy Location"].use_z = False
+    bpy.context.object.constraints["Copy Location"].target = root
+    bpy.context.object.constraints["Copy Location"].subtarget = "mixamorig:Hips"
+
+    bpy.ops.object.constraint_add(type='COPY_ROTATION')
+    bpy.context.object.constraints["Copy Rotation"].target = root
+    bpy.context.object.constraints["Copy Rotation"].subtarget = "mixamorig:Hips"
+    bpy.context.object.constraints["Copy Rotation"].use_y = False
+    bpy.context.object.constraints["Copy Rotation"].use_x = False
+
+    bpy.ops.nla.bake(frame_start=framerange[0], frame_end=framerange[1], step=1, only_selected=True, visual_keying=True, clear_constraints=True, clear_parents=False, use_current_action=False, bake_types={'OBJECT'})
+
+    #Create helper to bake hipmotion in Worldspace
+    bpy.ops.object.empty_add(type='PLAIN_AXES', radius=1, view_align=False, location=(0, 0, 0), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
+    hipsBaker = bpy.context.object
+    hipsBaker.name = "hipsBaker"
+
+    bpy.ops.object.constraint_add(type='COPY_LOCATION')
+    bpy.context.object.constraints["Copy Location"].target = root
+    bpy.context.object.constraints["Copy Location"].subtarget = "mixamorig:Hips"
+
+    bpy.ops.object.constraint_add(type='COPY_ROTATION')
+    bpy.context.object.constraints["Copy Rotation"].target = root
+    bpy.context.object.constraints["Copy Rotation"].subtarget = "mixamorig:Hips"
+
+    bpy.ops.nla.bake(frame_start=framerange[0], frame_end=framerange[1], step=1, only_selected=True, visual_keying=True, clear_constraints=True, clear_parents=False, use_current_action=False, bake_types={'OBJECT'})
+
+    #Bake Root motion to Armature (root)
+    root.select = True
+    bpy.context.scene.objects.active = root
+
+    bpy.ops.object.constraint_add(type='COPY_LOCATION')
+    bpy.context.object.constraints["Copy Location"].target = rootBaker
+
+    bpy.ops.object.constraint_add(type='COPY_ROTATION')
+    bpy.context.object.constraints["Copy Rotation"].target = bpy.data.objects["rootBaker"]
+    bpy.context.object.constraints["Copy Rotation"].use_offset = True
+
+    bpy.ops.nla.bake(frame_start=framerange[0], frame_end=framerange[1], step=1, only_selected=True, visual_keying=True, clear_constraints=True, clear_parents=False, use_current_action=True, bake_types={'OBJECT'})
+
+
+    bpy.ops.object.mode_set(mode = 'POSE')
+    hips.bone.select = True
+    root.data.bones.active = hips.bone
+
+    bpy.ops.pose.constraint_add(type='COPY_LOCATION')
+    hips.constraints["Copy Location"].target = hipsBaker
+    bpy.ops.pose.constraint_add(type='COPY_ROTATION')
+    hips.constraints["Copy Rotation"].target = hipsBaker
+
+    bpy.ops.nla.bake(frame_start=framerange[0], frame_end=framerange[1], step=1, only_selected=True, visual_keying=True, clear_constraints=True, clear_parents=False, use_current_action=True, bake_types={'POSE'})
+
+    #Delete helpers
+    bpy.ops.object.mode_set(mode = 'OBJECT')
+    bpy.ops.object.select_all(action='DESELECT')
+
+    hipsBaker.select = True
+    rootBaker.select = True
+
+    bpy.ops.object.delete(use_global=False)
+    
+#End of HipToRoot Function
+
+'''
+Batch Convert MixamoRigs
+'''
+def BatchHipToRoot(source_dir, dest_dir, use_z = True, on_ground = True):
+    numfiles = 0
+    for file in os.scandir(source_dir):
+        if file.name[-4::] == ".fbx":
+            numfiles += 1
+            bpy.ops.object.select_all(action='SELECT')
+            bpy.ops.object.delete(use_global=True)
+            #remove all datablocks
+            for mesh in bpy.data.meshes:
+                bpy.data.meshes.remove(mesh, do_unlink=True)
+            for material in bpy.data.materials:
+                bpy.data.materials.remove(material, do_unlink=True)
+            for action in bpy.data.actions:
+                    bpy.data.actions.remove(action, do_unlink=True)
+            #imprt FBX
+            bpy.ops.import_scene.fbx(filepath=file.path, axis_forward='-Z', axis_up='Y', directory="", filter_glob="*.fbx", ui_tab='MAIN', use_manual_orientation=False, global_scale=1, bake_space_transform=False, use_custom_normals=True, use_image_search=True, use_alpha_decals=False, decal_offset=0, use_anim=True, anim_offset=1, use_custom_props=True, use_custom_props_enum_as_string=True, ignore_leaf_bones=True, force_connect_children=False, automatic_bone_orientation=False, primary_bone_axis='Y', secondary_bone_axis='X', use_prepost_rot=True)
+            def getArmature(objects):
+                for a in objects:
+                    if a.type == 'ARMATURE':
+                        return a
+            armature = getArmature(bpy.context.selected_objects)
+            #armature = bpy.data.objects["Armature"]
+            mesh_obj = armature.children[0]
+            #do hip to Root conversion
+            HipToRoot(armature)
+            #remove newly created orphan actions
+            for action in bpy.data.actions:
+                if action != armature.animation_data.action:
+                    bpy.data.actions.remove(action, do_unlink=True)
+            '''
+            for mesh in bpy.data.meshes:
+                if mesh != mesh_obj.data:
+                    bpy.data.meshes.remove(mesh, do_unlink=True)
+            
+            for material in bpy.data.materials:
+                if material.users == 0:
+                    bpy.data.materials.remove(material, do_unlink=True)
+            '''
+            bpy.ops.export_scene.fbx(filepath=dest_dir + file.name, use_selection=False)
+            bpy.ops.object.select_all(action='SELECT')
+            bpy.ops.object.delete(use_global=False)
+            print("%d files converted" % numfiles)
+    return numfiles
+
+if __name__ == "__main__":
+    print("mixamoconv Hello.")
